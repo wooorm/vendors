71d97959c1e63c17d2db9aa5aebeb544e5e733d8
Titus Wormer <tituswormer@gmail.com>
2016-11-19 20:01:43 +0100

    Remove superfluous comments

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module vendors
- * @fileoverview Test suite for `vendors`.
- */
-
 'use strict';
 
-/* Module dependencies. */
 var test = require('tape');
 var vendors = require('./');
 
-/* Tests. */
 test('vendors', function (t) {
   t.ok(Array.isArray(vendors), 'should be an `array`');
 
