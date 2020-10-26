d265cc5da37c248b4368f2dcb6c42c18cd599f1a
Titus Wormer <tituswormer@gmail.com>
2020-10-26 12:03:13 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,10 +3,10 @@
 var test = require('tape')
 var vendors = require('.')
 
-test('vendors', function(t) {
+test('vendors', function (t) {
   t.ok(Array.isArray(vendors), 'should be an `array`')
 
-  vendors.forEach(function(vendor) {
+  vendors.forEach(function (vendor) {
     t.equal(typeof vendor, 'string', '`' + vendor + '` should be a string')
   })
 
